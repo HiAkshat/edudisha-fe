b4f6e198f0ddc15454d255e825a9601a2a034c60
akshat-blitz <akshatgupta300@gmail.com>
2025-04-16 17:22:54 +0530

    init: repo setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+function App() {
+  return (
+    <>
+      <div className='bg-red-500'>Hello</div>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
