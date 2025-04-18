94a4a380a4a0e4dfafb524d23ae7eaa873c5d14a
akshat-blitz <akshatgupta300@gmail.com>
2025-04-18 12:47:16 +0530

    add: zigzag pattern

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import SideNav from "./SideNav/SideNav";
 import React, { PropsWithChildren } from "react";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { CalendarIcon, SearchIcon, SettingsIcon } from "lucide-react";
 import { InboxIcon } from "lucide-react";
 import { HomeIcon } from "lucide-react";
@@ -33,24 +33,29 @@ const items = [
     url: "#",
     icon: SettingsIcon,
   },
-]
+];
 
 const BaseLayout = ({ children }: PropsWithChildren): React.ReactElement => {
   const [isSidebarHovered, setIsSidebarHovered] = React.useState(false);
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="bg-[var(--color-error-50)] dark:bg-gray-950">
-        <SidebarProvider className="flex gap-3" defaultOpen={false} open={isSidebarHovered}>
-          <SideNav items={items} setIsSidebarHovered={setIsSidebarHovered} />
-          
-            
-          <div className="flex flex-col gap-2 w-full p-2">
+      <div className="bg-[var(--color-warning-100)] dark:bg-gray-950 relative min-h-screen">
+        <div className="absolute inset-0 bg-[url('/zigzag2.png')] bg-cover bg-center opacity-90 dark:opacity-10 pointer-events-none image-blend" />
+        <div className="relative z-10">
+          <SidebarProvider
+            className="flex gap-3"
+            defaultOpen={false}
+            open={isSidebarHovered}
+          >
+            <SideNav items={items} setIsSidebarHovered={setIsSidebarHovered} />
 
-            <Navbar />
-            {children}
-          </div>
-        </SidebarProvider>
+            <div className="flex flex-col gap-2 w-full p-2">
+              <Navbar />
+              {children}
+            </div>
+          </SidebarProvider>
+        </div>
       </div>
     </ThemeProvider>
   );
